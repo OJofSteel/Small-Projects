a5fc945258c4cba5d40e5d30d67dec0681ceaa9e
OJ of Steel <OJofSteelNGU@gmail.com>
2021-01-21 18:47:09 -0500

    Added timing to different reverse string functions in reverse_string.py

--- reverse_string.py
+++ reverse_string.py
@@ -1,4 +1,5 @@
 import sys
+import timeit
 
 from PyQt5 import QtWidgets as qtw
 
@@ -67,6 +68,11 @@ def reverse(string):
     return string
 
 
+def time_function(function_name, text):
+    times = timeit.timeit(lambda: function_name(text), number=10000)
+    return "{:2f}".format(times) + "s"
+
+
 class MainApp(qtw.QApplication):
 
     def __init__(self, argv):
@@ -81,13 +87,26 @@ class MainWindow(qtw.QWidget):
 
     def __init__(self):
         super().__init__()
+
+        self.slice_time_text = "Time to slice: "
+        self.loop_time_text = "Time to loop: "
+        self.recursion_time_text = "Time to recurse: "
+        self.stack_time_text = "Time to stack: "
+        self.reversed_time_text = "Time to reverse(): "
+
         self.info_label = qtw.QLabel("Please Enter Text")
-        self.input_string = qtw.QLabel()
-        self.reversed_sliced = qtw.QLabel()
-        self.reversed_loop = qtw.QLabel()
-        self.reversed_recursion = qtw.QLabel()
-        self.reversed_stack = qtw.QLabel()
-        self.reversed_reversed = qtw.QLabel()
+        self.input_string = qtw.QLabel("Inputted Text: ")
+        self.reversed_sliced = qtw.QLabel('Reversed using slicing: ')
+        self.reversed_loop = qtw.QLabel("Reversed using loop: ")
+        self.reversed_recursion = qtw.QLabel("Reversed using recursion: ")
+        self.reversed_stack = qtw.QLabel("Reversed using stack: ")
+        self.reversed_reversed = qtw.QLabel("Reversed using reverse(): ")
+
+        self.slice_time = qtw.QLabel(self.slice_time_text)
+        self.loop_time = qtw.QLabel(self.loop_time_text)
+        self.recursion_time = qtw.QLabel(self.recursion_time_text)
+        self.stack_time = qtw.QLabel(self.stack_time_text)
+        self.reversed_time = qtw.QLabel(self.reversed_time_text)
 
         self.setWindowTitle("Reverse String")
         self.setMinimumSize(250, 50)
@@ -102,22 +121,34 @@ class MainWindow(qtw.QWidget):
         self.layout().addWidget(self.reversed_recursion)
         self.layout().addWidget(self.reversed_stack)
         self.layout().addWidget(self.reversed_reversed)
+        self.layout().addWidget(self.slice_time)
+        self.layout().addWidget(self.loop_time)
+        self.layout().addWidget(self.recursion_time)
+        self.layout().addWidget(self.stack_time)
+        self.layout().addWidget(self.reversed_time)
 
         self.input.textChanged.connect(self.inputted_logic)
+        self.input.editingFinished.connect(self.edited)
 
     def inputted_logic(self):
         self.inputted_string = self.input.text()
-        self.input_string.setText("Inputted String: " + self.inputted_string)
-        self.reversed_sliced.setText('Reversed using Slicing: ' + reverse_string_slice(self.inputted_string))
+        self.input_string.setText("Inputted Text: " + self.inputted_string)
+        self.reversed_sliced.setText('Reversed using slicing: ' + reverse_string_slice(self.inputted_string))
         self.reversed_loop.setText("Reversed using loop: " + reverse_string_loop(self.inputted_string))
         self.reversed_recursion.setText("Reversed using recursion: " + reverse_string_recursion(self.inputted_string))
         self.reversed_stack.setText("Reversed using stack: " + reverse_string_stack(self.inputted_string))
         self.reversed_reversed.setText("Reversed using reverse(): " + reverse(self.inputted_string))
         self.adjustSize()
 
+    def edited(self):
+        self.slice_time.setText(self.slice_time_text + time_function(reverse_string_slice, self.inputted_string))
+        self.loop_time.setText(self.loop_time_text + time_function(reverse_string_loop, self.inputted_string))
+        self.recursion_time.setText(self.recursion_time_text + time_function(reverse_string_recursion,
+                                                                             self.inputted_string))
+        self.stack_time.setText(self.stack_time_text + time_function(reverse_string_stack, self.inputted_string))
+        self.reversed_time.setText(self.reversed_time_text + time_function(reverse, self.inputted_string))
+
 
 if __name__ == '__main__':
     app = MainApp(sys.argv)
     sys.exit(app.exec_())
-
-# Function that reverses the string given
